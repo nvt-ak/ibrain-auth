10aabadb265b27bbb9c21b4b59e51e62f7b3b0e4
John Nguyen <john@techfox.io>
2022-01-11 12:47:15 +0700

    upgrade version

--- app/graphql/ibrain/auth/mutations/sso_sign_in_mutation.rb
+++ app/graphql/ibrain/auth/mutations/sso_sign_in_mutation.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Ibrain::Auth::Mutations
-  class SignInMutation < BaseMutation
+  class SsoSiginInMutation < BaseMutation
     field :user, Types::Objects::UserType, null: true
     field :token, String, null: true
     field :result, Boolean, null: true
--- lib/ibrain/auth/version.rb
+++ lib/ibrain/auth/version.rb
@@ -4,14 +4,14 @@ module Ibrain
   # frozen_string_literal: true
 
   module Auth
-    VERSION = '0.1.5'
+    VERSION = '0.1.6'
 
     def self.ibrain_auth_version
       VERSION
     end
 
     def self.previous_ibrain_auth_minor_version
-      '0.1.4'
+      '0.1.5'
     end
 
     def self.ibrain_auth_gem_version
