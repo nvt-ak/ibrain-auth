a462b2556f86e841470d93c1da7c8a5620beebd8
John Nguyen <john@techfox.io>
2022-01-14 17:43:36 +0700

    fix allowed roles

--- lib/ibrain/auth/version.rb
+++ lib/ibrain/auth/version.rb
@@ -4,14 +4,14 @@ module Ibrain
   # frozen_string_literal: true
 
   module Auth
-    VERSION = '0.1.8'
+    VERSION = '0.1.9'
 
     def self.ibrain_auth_version
       VERSION
     end
 
     def self.previous_ibrain_auth_minor_version
-      '0.1.7'
+      '0.1.8'
     end
 
     def self.ibrain_auth_gem_version
