c2f126bdadb79442436e075b63aaa0349db37085
Tai Nguyen <tainv@its-global.vn>
2022-10-17 11:18:05 +0700

    refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
-Ibrain::Core::Engine.routes.draw do
+Ibrain::Auth::Engine.routes.draw do
   devise_for :users, controllers: {
     sessions: "ibrain/auth/sessions",
     registrations: "ibrain/auth/registrations",
-    callback: "ibrain/auth/sessions/callback"
     },
     path: "api/#{Ibrain::Config.api_version}/users",
     defaults: { format: :json }
+
+  get "api/#{Ibrain::Config.api_version}/users/callback" => 'sessions#callback'
 end
--- lib/ibrain/auth/version.rb
+++ lib/ibrain/auth/version.rb
@@ -4,14 +4,14 @@ module Ibrain
   # frozen_string_literal: true
 
   module Auth
-    VERSION = '0.3.2'
+    VERSION = '0.3.3'
 
     def self.ibrain_auth_version
       VERSION
     end
 
     def self.previous_ibrain_auth_minor_version
-      '0.3.1'
+      '0.3.2'
     end
 
     def self.ibrain_auth_gem_version
